28bc376e5d4e3b035a0aabba73fa6e50771f7ea6
Florian Pgn <florianpgn@gmail.com>
2016-10-14 09:38:44 +0200

    Base program

---
+++ src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Integer nbBinaire = 26684 & 0x1ff;
+		
+		System.out.println(nbBinaire);
+		
+	}
+
+}
